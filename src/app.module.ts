38c9ccd9be9cd3a59352e45da386b4032707c4b4
Francisco Amador <isco.amaador@gmail.com>
2025-01-23 16:35:23 -0600

    Configure TypeORM with SQLite and import Food, Patient, and Reminder modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FoodModule } from './food/food.module';
+import { PatientModule } from './patient/patient.module';
+import { ReminderModule } from './reminder/reminder.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logger: 'advanced-console',
+      logging: 'all',
+    }),
+    FoodModule,
+    PatientModule,
+    ReminderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
